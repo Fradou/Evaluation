4b506f5904c08bc90804efe6613a1c919598a30e
Alexandre FRADET <a_fradet@yahoo.fr>
2016-11-14 09:58:11 +0100

    Create Commentaire Entity

---
+++ src/MediaBundle/Entity/Commentaire.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MediaBundle\Entity;
+
+/**
+ * Commentaire
+ */
+class Commentaire
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $utilisateur;
+
+    /**
+     * @var string
+     */
+    private $comment;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set utilisateur
+     *
+     * @param string $utilisateur
+     *
+     * @return Commentaire
+     */
+    public function setUtilisateur($utilisateur)
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get utilisateur
+     *
+     * @return string
+     */
+    public function getUtilisateur()
+    {
+        return $this->utilisateur;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return Commentaire
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
+
